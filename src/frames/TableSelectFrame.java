aadaf45fb7cf2ca7803ce932de85b83beda1d3dc
Jakub Sarnowski <j.sarnowski@travactory.com>
2020-05-10 19:37:22 +0200

    fixed default position of TableSelectFrame

--- src/frames/TableSelectFrame.java
+++ src/frames/TableSelectFrame.java
@@ -42,7 +42,7 @@ public class TableSelectFrame extends javax.swing.JFrame {
                 initComponents();
 
         this.setTitle("Main menu");
-
+        setLocationRelativeTo(null);
         ResultSet rs = main.transactions.GetDBList();
         List<String> databases = new ArrayList<String>();
         
